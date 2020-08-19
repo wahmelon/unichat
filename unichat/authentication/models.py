98eb5deef615bab5025aba29ee5738b5a77eeeeb
Max Singer <wahmelon@gmail.com>
2020-08-20 03:16:39 +1000

    add comment

--- unichat/authentication/models.py
+++ unichat/authentication/models.py
@@ -3,5 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class StudentUser(AbstractUser):
 	current_units = models.CharField(blank=True, max_length=120)
+
+	#to add: alias
 	
 # Create your models here.
